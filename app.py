e4f8689566bddd3b444e7e72c7b58472869383b1
KritikaTiwari <84006402+KritikaTiwari@users.noreply.github.com>
2022-08-02 09:09:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
-pickle_in = open("decision_model.pkl","rb")
+pickle_in = open("/content/drive/My Drive/decision1_model.pkl","rb")
 model=pickle.load(pickle_in)
-dataset= pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X = sc.fit_transform(X)
+dataset= pd.read_csv('/content/drive/My Drive/Social_Network_Ads.csv')
+X = dataset.iloc[:, [1, 2, 3]].values
+from sklearn.preprocessing import LabelEncoder
+labelencoder_1=LabelEncoder()
+X[:,0]=labelencoder_1.fit_transform(X[:,0])
 def predict_note_authentication(UserID, Gender,Age,EstimatedSalary):
-  output= model.predict(sc.transform([[Age,EstimatedSalary]]))
+  output= model.predict([[Gender,Age,EstimatedSalary]])
   print("Purchased", output)
   if output==[1]:
     prediction="Item will be purchased"
@@ -23,35 +23,42 @@ def predict_note_authentication(UserID, Gender,Age,EstimatedSalary):
     prediction="Item will not be purchased"
   print(prediction)
   return prediction
+  
 def main():
     
     html_temp = """
-   <div class="" style="background-color:blue;" >
+   
+    
+   <div class="" style="background-color:yellow;" >
    <div class="clearfix">           
    <div class="col-md-12">
-   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
-   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
-   <center><p style="font-size:25px;color:white;margin-top:10px;">Internship Project Deployment</p></center> 
+   <center><p style="font-size:30px;color:Black;margin-top:10px;">Department of Computer Engineering</p></center> 
+   <center><p style="font-size:40px;color:Black;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
+   <center><p style="font-size:25px;color:Black;margin-top:10px;">Internship Project Deployment</p></center> 
    </div>
    </div>
    </div>
    """
     st.markdown(html_temp,unsafe_allow_html=True)
     st.header("Item Purchase Prediction")
-    UserID = st.text_input("UserID","")
-    Gender = st.selectbox('Gender',('Male', 'Female', 'Others'))
-    Age = st.number_input("Insert Age",18,60)
+    UserID = st.text_input("UserID"," ")
+    Gender= st.radio('Gender',('Male', 'Female'))
+    if Gender== "Male":
+        Gender=1
+    else:
+        Gender=0
+
+
+    Age=st.number_input("Insert Age",18,60)
     EstimatedSalary = st.number_input("Insert salary",15000,150000)
     resul=""
     if st.button("Predict"):
       result=predict_note_authentication(UserID, Gender,Age,EstimatedSalary)
       st.success('Model has predicted {}'.format(result))
-    if st.button("About"):
-      st.subheader("Developed by Deepak Moud")
-      st.subheader("Head , Department of Computer Engineering")
+      st.subheader("Developed by Kritika Tiwari")
+      st.subheader("Student, Poornima Institute of Engineering and Technology")
+      
 
 if __name__=='__main__':
   main()
    
-
-
